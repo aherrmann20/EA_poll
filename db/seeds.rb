199d7566d6d9e62ecfccce3234f24d7eea6b2908
aherrmann20 <aherrmann@college.harvard.edu>
2015-12-05 12:49:40 -0500

    Seeded database and added admin capabilities

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,29 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+puts "Creating admin..."
+
+admin = User.create! email: "admin@example.com", password: "password", password_confirmation: "password", admin: true
+
+puts "Creating user..."
+
+user = User.create! email: "email@email.com", password: "password", password_confirmation: "password"
+
+Poll.create! topic: "Will we succeed in our quest to do well in CS50?" do |poll|
+	poll.vote_options << VoteOption.find_or_create_by!(title: "Yes")
+	poll.vote_options << VoteOption.find_or_create_by!(title: "No (cries)")
+	poll.vote_options << VoteOption.find_or_create_by!(title: "HELL YES")
+end
+
+Poll.create! topic: "Is David Malan a badass?" do |poll|
+	poll.vote_options << VoteOption.find_or_create_by!(title: "Yes")
+	poll.vote_options << VoteOption.find_or_create_by!(title: "Maybe")
+	poll.vote_options << VoteOption.find_or_create_by!(title: "Of course")
+end
+
+Poll.create! topic: "Does Diet Coke cause cancer?" do |poll|
+	poll.vote_options << VoteOption.find_or_create_by!(title: "Probably")
+	poll.vote_options << VoteOption.find_or_create_by!(title: "I'm pretty sure it does")
+	poll.vote_options << VoteOption.find_or_create_by!(title: "I'm gonna drink it anyway")
+end
