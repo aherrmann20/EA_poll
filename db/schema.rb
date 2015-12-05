e2b56dd8dc8614db64852ca521713c546d36d78a
aherrmann20 <aherrmann@college.harvard.edu>
2015-12-05 16:31:16 -0500

    About to manually edit migration... wish me luck.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151205172716) do
+ActiveRecord::Schema.define(version: 20151205210828) do
 
   create_table "admins", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
@@ -33,8 +33,9 @@ ActiveRecord::Schema.define(version: 20151205172716) do
 
   create_table "polls", force: :cascade do |t|
     t.text     "topic"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at",                null: false
+    t.datetime "updated_at",                null: false
+    t.boolean  "open",       default: true
   end
 
   create_table "profiles", force: :cascade do |t|
